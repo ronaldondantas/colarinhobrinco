8b6423d2a50d667b6319e12eb3678b5618d98cd7
Caio H. Costa <caiohc@gmail.com>
2017-05-02 17:20:30 -0300

    First project version

---
+++ src/main/java/colarinhobranco/web/news/ListServlet.java
@@ -0,0 +1,36 @@
+package colarinhobranco.web.news;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import colarinhobranco.dao.NewsDao;
+import colarinhobranco.daoimpl.NewsDaoImpl;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns="/news/list")
+public class ListServlet extends HttpServlet {
+	
+	private NewsDao newsDao = new NewsDaoImpl();
+		
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		
+		request.setAttribute("news", newsDao.findAll());
+		
+		request.getRequestDispatcher("/pages/news/list.jsp").forward(request, response);
+		
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		
+		doGet(request, response);
+		
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/snippets/site-navbar.jsp
@@ -0,0 +1,7 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<nav id="site-navbar">
+	<ul>
+		<li><a href="<c:url value='/news/list'/>">Notícias</a></li>
+		<li><a href="<c:url value='/pages/news/edit.jsp'/>">Enviar</a></li>			
+	</ul>
+</nav>
